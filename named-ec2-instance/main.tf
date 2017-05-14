847ebe7b0fbdb665ad80202863320411998c1c6b
Tim Lieberman <tim@timdev.com>
2017-05-14 15:55:18 -0700

    debugging root_block_device

--- named-ec2-instance/main.tf
+++ named-ec2-instance/main.tf
@@ -6,10 +6,10 @@ resource "aws_instance" "server" {
   ami                    = "${var.ami_id}"
   instance_type          = "${var.instance_type}"
   iam_instance_profile   =  "${var.instance_profile_name}"
+  root_block_device      = "${var.root_block_device}"
   key_name               = "${var.key_name}"
   subnet_id              = "${var.subnet_id}"
   vpc_security_group_ids =  ["${var.vpc_security_group_ids}"]
-  root_block_device      = ["${var.root_block_device}"]
   tags                   = "${var.tags}"
   volume_tags            = "${var.volume_tags}"
 
